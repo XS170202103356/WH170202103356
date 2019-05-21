d2656968dc78e3ca6550c4ddbbb7efec40da9ba5
XS170202103356 <379328869@qq.com>
2019-05-21 16:49:43 +0800

    提交demoProj01

--- src/demoProj/jiaowu.java
+++ src/demoProj/jiaowu.java
@@ -1,7 +1,6 @@
- package demoProj;
-			
- public class jiaowu {
-       public static void main(String[] args) {
-			System.out.println("good!");
+package demoProj;
+			public class jiaowu {
+				public static void main(String[] args) {
+					System.out.println("good branch!");
 				}
 }
